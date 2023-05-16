212cfea2bb30d4293cc897e46cbe3dbd0d8789e8
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-05-16 19:25:59 +0000

    Format C/C++ code with clang-format

--- src/main.c
+++ src/main.c
@@ -15,8 +15,7 @@
 
 #define VERSION "1.0.0"
 
-void print_help()
-{
+void print_help() {
     printf("Usage: strings [options] [file]\n");
     print("Strings is a command line tool that prints all printable strings "
           "that are found in a file.\n");
@@ -26,39 +25,32 @@ void print_help()
     printf("  -v, --version\t\t\tShow version\n");
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     bool show_line_numbers = false;
-    if (argc == 1)
-    {
+    if (argc == 1) {
         printf("Usage: strings [options] [file]\n");
         return 1;
     }
 
-    if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0)
-    {
+    if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
         printf("strings version %s\n", VERSION);
         return 0;
     }
 
-    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
-    {
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
         print_help();
         return 0;
     }
 
-    if (argc > 2)
-    {
+    if (argc > 2) {
         if (strcmp(argv[2], "-ln") == 0 ||
-            strcmp(argv[2], "--line-numbers") == 0)
-        {
+            strcmp(argv[2], "--line-numbers") == 0) {
             show_line_numbers = true;
         }
     }
 
     FILE *file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
+    if (file == NULL) {
         printf("Error: File not found\n");
         return 1;
     }
@@ -67,30 +59,24 @@ int main(int argc, char *argv[])
     size_t read;
     int line_number = 0;
 
-    while ((read = getline(&line, &len, file)) != -1)
-    {
+    while ((read = getline(&line, &len, file)) != -1) {
         line_number++;
-        for (int i = 0; i < read; i++)
-        {
+        for (int i = 0; i < read; i++) {
             int printable_chars = 0;
             int start_index = i;
-            while (isprint(line[i]))
-            {
+            while (isprint(line[i])) {
                 printable_chars++;
                 i++;
             }
 
-            if (printable_chars > 2)
-            {
-                if (show_line_numbers)
-                {
+            if (printable_chars > 2) {
+                if (show_line_numbers) {
                     printf("%s%d%s | %s", ANSI_COLOR_CYAN, line_number,
                            ANSI_COLOR_GRAY, ANSI_COLOR_RESET);
                 }
 
                 for (int j = start_index; j < start_index + printable_chars;
-                     j++)
-                {
+                     j++) {
                     printf("%c", line[j]);
                 }
                 printf("\n");
