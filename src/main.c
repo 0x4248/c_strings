08b6b99501b4365b59ec36818be510e8c2c6c6b0
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-11 11:57:07 +0100

    Username update
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 /* C Strings
  * The strings command written in C
- * Github: https://www.github.com/lewisevans2007/c_strings
+ * Github: https://www.github.com/0x4248/c_strings
  * Licence: GNU General Public License v3.0
- * By: Lewis Evans
+ * By: 0x4248
  */
 
 #include <ctype.h>
